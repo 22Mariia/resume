1a5df12c1bd794206fb3e064080f1de8776ef1c2
Mariia <125997069+22Mariia@users.noreply.github.com>
2023-05-19 22:13:27 +0300

    Update index.js

--- src/route/index.js
+++ src/route/index.js
@@ -84,7 +84,7 @@ router.get('/', function (req, res) {
                  },
                  {
                   text: 'shophome',
-                  link: '/shophomer',
+                  link: '/shophome',
                    },
                    {
                     text: 'shoporder',
@@ -92,7 +92,7 @@ router.get('/', function (req, res) {
                      },
                      {
                       text: 'shopreview',
-                      link: '/shopreviewr',
+                      link: '/shopreview',
                        },
                        {
                         text: 'task21',
